34fd671ddb3b27305ddae208e38d404497e215cd
Joann Ellis <joannpellis@gmail.com>
2024-09-11 10:59:34 -0400

    Initial commit

---
+++ src/main/java/org/joann/springsecurityapi2/SpringSecurityApi2Application.java
@@ -0,0 +1,13 @@
+package org.joann.springsecurityapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/joann/springsecurityapi2/SpringSecurityApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.joann.springsecurityapi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
